cd1ad9cd74e867fa4f5846c72f2bab9fb8c2b808
Pascal P <pascal.phelipot@gmail.com>
2015-11-17 17:08:51 +0100

    Quelques détails encore

--- src/vue/inscription.php
+++ src/vue/inscription.php
@@ -3,50 +3,49 @@
 
 
 <form id="inscription" method="post" action="#">
-		<div>
-			<label for="pseudoJoueur"> Votre Pseudo: </label>
-			<input type="text" name="pseudoJoueur" id="pseudoJoueur"/>
-			<label for="mdpJoueur">Mot de Passe </label>
-			<input type="password" name="mdpJoueur" id="mdpJoueur" />
-			<label for="mdp2Joueur">Confirmation </label>
-			<input type="password" name="mdp2Joueur" id="mdp2Joueur"/>
-		</div>
+	<input type="hidden" name="page" value="inscription" />
+	<div>
+		<label for="pseudoJoueur"> Votre Pseudo: </label>
+		<input type="text" name="pseudoJoueur" id="pseudoJoueur"/>
+		<label for="mdpJoueur">Mot de Passe </label>
+		<input type="password" name="mdpJoueur" id="mdpJoueur" />
+		<label for="mdp2Joueur">Confirmation </label>
+		<input type="password" name="mdp2Joueur" id="mdp2Joueur"/>
+	</div>
 
-		<div>
-			<label for="mailJoueur">Mail Joueur:</label>
-			<input type="email" name="mailJoueur" id="mailJoueur"/>
-		</div>
+	<div>
+		<label for="mailJoueur">Mail Joueur:</label>
+		<input type="email" name="mailJoueur" id="mailJoueur"/>
+	</div>
 
-		<div>
-			<label for="sexeJoueur">Sexe: </label>
-			<input type="radio" name="sexe" value="homme"><label>homme</label>
-			<input type="radio" name="sexe" value="femme"><label>femme</label>
-			<label>Date de naissance: </label>
-			<input type="text" id="jour" maxlength="2" size="2" value="jour"/>
-			<select name="mois">
-				<optgroup>
-					<option value="1" >Janvier</option>
-					<option value="2" >Février</option>
-					<option value="3" >Mars</option>
-					<option value="4" >Avril</option>
-					<option value="5" >Mai</option>
-					<option value="6" >Juin</option>
-					<option value="7" >Juillet</option>
-					<option value="8" >Aout</option>
-					<option value="9" >Septembre</option>
-					<option value="10" >Octobre</option>
-					<option value="11" >Novembre</option>
-					<option value="12" >Décembre</option>
-				</optgroup>
-<input type="text" name="annee" maxlength="4" size="4" value="annee"/>	
-</select>
-		</div>
+	<div>
+		<label for="sexeJoueur">Sexe: </label>
+		<input type="radio" name="sexe" value="homme"><label>homme</label>
+		<input type="radio" name="sexe" value="femme"><label>femme</label>
+		<label>Date de naissance: </label>
+		<input type="text" id="jour" maxlength="2" size="2" value="jour"/>
+		<select name="mois">
+			<optgroup>
+				<option value="1" >Janvier</option>
+				<option value="2" >Février</option>
+				<option value="3" >Mars</option>
+				<option value="4" >Avril</option>
+				<option value="5" >Mai</option>
+				<option value="6" >Juin</option>
+				<option value="7" >Juillet</option>
+				<option value="8" >Aout</option>
+				<option value="9" >Septembre</option>
+				<option value="10" >Octobre</option>
+				<option value="11" >Novembre</option>
+				<option value="12" >Décembre</option>
+			</optgroup>
+		</select>
+		<input type="text" name="annee" maxlength="4" size="4" value="annee"/>	
+	</div>
 
-
-		<div>
-			<input type="submit" name="inscription" value="S'inscrire"/>
-			<input type="reset" name="annuler" value="Annuler"/>
-		</div>
+	<div>
+		<input type="submit" value="Valider" />
+	</div>
 </form>
 
 
