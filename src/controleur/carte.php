de1f298cad939c0577f75b64f53eee36df3d0f48
kenta77 <quentin.rimoux@live.fr>
2015-11-18 14:46:28 +0100

    Pour la carte (non j'ai pas vraiment commencé :|)

---
+++ src/controleur/carte.php
@@ -0,0 +1,5 @@
+<?php 
+	$titre = 'Carte du monde';
+	include_once('./vue/carte.php');
+
+?>
---
+++ src/vue/carte.php
@@ -0,0 +1,8 @@
+<?php
+	include_once('/vue/header.php');
+?>
+
+
+<?php
+	include_once('/vue/footer.php');
+?>
