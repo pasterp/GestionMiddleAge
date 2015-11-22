3a4d6acc83c41566195df11ea8884cc59fa603ec
Pascal P <pascal.phelipot@gmail.com>
2015-11-22 22:00:54 +0100

    Début de POO (objet Ressource) Début du doc en LaTeX (structure)

---
+++ src/modele/ressource.php
@@ -0,0 +1,20 @@
+	<?php 
+
+include_once('modele/connexion_sql.php');
+
+	class Ressource {
+		private $id;
+		private $nom;
+		private $description;
+
+
+		public static function Ressources(){
+			//Methode statique retournant toutes les resosurces existantes
+			$req = 'SELECT * FROM RESSOURCE';
+			global $bdd;
+			$req = $bdd->query($req);
+			return $req->fetchAll();
+		}
+
+		
+}
