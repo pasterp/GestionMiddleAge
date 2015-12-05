ad5003bc0a3286a94b4a590bbdeb329d519ac348
Mahatehotia <mahatehotia@hotmail.com>
2015-12-05 01:30:43 +0100

    template unite.php dans vue

--- src/vue/Unite.php
+++ src/vue/Unite.php
@@ -1,38 +1,26 @@
 <?php include_once('vue/header.php'); ?>
 
-
-<div class="row collapse">
-  <div class="medium-3 columns">
-    <ul class="tabs vertical" id="example-vert-tabs" data-tabs>
-      <li class="tabs-title is-active"><a href="#panel1v" aria-selected="true">Tab 1</a></li>
-      <li class="tabs-title"><a href="#panel2v">Tab 2</a></li>
-      <li class="tabs-title"><a href="#panel3v">Tab 3</a></li>
-      <li class="tabs-title"><a href="#panel4v">Tab 4</a></li>
-      <li class="tabs-title"><a href="#panel3v">Tab 5</a></li>
-      <li class="tabs-title"><a href="#panel4v">Tab 6</a></li>
-    </ul>
-    </div>
-    <div class="medium-9 columns">
-    <div class="tabs-content vertical" data-tabs-content="example-vert-tabs">
-      <div class="tabs-panel is-active" id="panel1v">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
-      <div class="tabs-panel" id="panel2v">
-        <p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p>
-      </div>
-      <div class="tabs-panel" id="panel3v">assets/img/rectangle-3.jpg">
-      </div>
-      <div class="tabs-panel" id="panel4v">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
-      <div class="tabs-panel" id="panel5v">
-        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-      <div class="tabs-panel" id="panel6v">
-        <img class="thumbnail" src="">
-      </div>
-    </div>
+<div class="row">
+<div class="small-6 large-12 columns">
+<div class="panel">
+<div class="row">
+  <h1>Nom Unité</h1>
+            <hr>
+          </div>
+          <div class="row">
+            <div class="small-4 columns">
+              <img src="http://placehold.it/200x200" /> </div>
+            <div class="small-8 columns">
+              <h4> Liste des ressources</h4>
+              <p>blabla</p>
+              <h5> Description:</h5>
+              <p>blabla</p>
+              <h5> Puissance:</h5>
+              <p>blabla</p>
+            </div>
+          </div>
   </div>
+</div>
 </div>
 
 <?php include_once('vue/footer.php'); ?>
