29121e3bc5b914d205eb0f8bdc7ff9ad0006cb7e
Mahatehotia <mahatehotia@hotmail.com>
2015-12-07 09:27:04 +0100

    modif de la page unite.php

--- src/controleur/unite.php
+++ src/controleur/unite.php
@@ -2,9 +2,31 @@
 	include_once('./modele/ressource.php');
 	include_once('./modele/Unite.php');
 
-	$titre="Mes unités";
-	$res = Ressource::Ressources();
+	if (estAuthentifier()) {
+    global $currentJoueur;
+    $uc = $currentJoueur->getIdUnite();
+    $titre="Mes Unités";
 	$uni = Unite::Unites();
+    $uniConnues = array();
+    $uniInconnues = array();
+    foreach ($uni as $u){
+        $b = false;
+        foreach($uc as $m){
+            if ($u->getIdUnite() == $m[0]){
+                $b = true;
+            }
+        }
+        if($b){
+            array_push($uniConnues, $uni);
+        }else{
+            array_push($uniInconnues, $uni);
+        }
+    }
+
+}else{
+    retourEnTerresConnues();
+}
+
 
 	include_once('./vue/Unite.php');
 ?>
